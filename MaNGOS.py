0af8231fb48480225bfdd79d1ab1eb90545282fe
Bjørn Slot <admin@isengard.dk>
2012-12-09 18:12:19 +0100

    Added options to use current installed databases..

--- MaNGOS.py
+++ MaNGOS.py
@@ -286,7 +286,7 @@ def MaNGOS_Database(host,user,password,work_dir,install_dir,version):
     else:
        for record in world_db:
         print "\nDatabases already exists!"
-        print "\nDelete Current databases?"
+        print "\nDelete Current databases or use Current databases?"
         del_db=Quest(0)
         if del_db=='yes' or 'Yes':
             world.execute('DROP database `mangos`')
@@ -296,31 +296,33 @@ def MaNGOS_Database(host,user,password,work_dir,install_dir,version):
             world.execute("DROP USER 'mangos'@'localhost'")
         else:
             print "\nInstaller is shuttingdown! Please Check the log file for more information!"
-            exit()
-    print "\nPreparing Databases..."
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/sql/create_mysql.sql')
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' characters < '+work_dir+'/server/sql/characters.sql')
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+work_dir+'/server/sql/mangos.sql')
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' realmd < '+work_dir+'/server/sql/realmd.sql')
-    if version=='tbc':
-       folder='scripts'
-    elif version=='classic':
-       folder='ScriptDevZero'
-    elif version=='cata':
-       folder='ScriptDev2'
-    elif version=='wotlk':
-       folder='ScriptDev2'
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_database.sql')
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_structure_mysql.sql')
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_script_full.sql')
-    print "\nCreateing full database.."
-    os.system('cd '+install_dir+'/database/database;sh make_full_db.sh')
-    os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+install_dir+'/database/database/full_db.sql')
-    if version=='tbc':
-       # Minor fix for TBC this is only temperary!
-       cursor = db.cursor()
-       cursor.execute('ALTER TABLE db_version CHANGE COLUMN required_12195_02_mangos_mangos_string required_s1718_12113_01_mangos_spell_template bit')
-    print "\nDatabase setup done."
+    if del_db=='yes' or del_db=='yes':    
+       print "\nPreparing Databases..."
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/sql/create_mysql.sql')
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' characters < '+work_dir+'/server/sql/characters.sql')
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+work_dir+'/server/sql/mangos.sql')
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' realmd < '+work_dir+'/server/sql/realmd.sql')
+       if version=='tbc':
+          folder='scripts'
+       elif version=='classic':
+          folder='ScriptDevZero'
+       elif version=='cata':
+          folder='ScriptDev2'
+       elif version=='wotlk':
+          folder='ScriptDev2'
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_database.sql')
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_structure_mysql.sql')
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_script_full.sql')
+       print "\nCreateing full database.."
+       os.system('cd '+install_dir+'/database/database;sh make_full_db.sh')
+       os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+install_dir+'/database/database/full_db.sql')
+       if version=='tbc':
+          # Minor fix for TBC this is only temperary!
+          cursor = db.cursor()
+          cursor.execute('ALTER TABLE db_version CHANGE COLUMN required_12195_02_mangos_mangos_string required_s1718_12113_01_mangos_spell_template bit')
+       print "\nDatabase setup done."
+     else:
+       print "\nSkipping, Reason: Using current installed databases!"
 
 def Quest(question):
     if question==0:
