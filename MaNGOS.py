44a3517cac301f4dd576308ec401aa0587888a58
Bjørn Slot <admin@isengard.dk>
2012-12-10 09:20:34 +0100

    modified:   .gitignore 	modified:   MaNGOS.py

--- MaNGOS.py
+++ MaNGOS.py
@@ -39,6 +39,15 @@ svn_ytdb='http://subversion.assembla.com/svn/ytdbase/'
 svn_udb='https://unifieddb.svn.sourceforge.net/svnroot/unifieddb'
 
 # Custom Repo
+# in dev
+git_custom_repo=[
+                 'mangchat',
+                 'gethostbyname',
+                 'Max_level_stackable_core',
+                 'TeleNPC2',
+                 'announce_kick'
+                ]
+
 git_mangchat='git://github.com/gimli/server.git mangchat'
 git_gethostbyname='git://github.com/gimli/server.git gethostbyname'
 git_Maxlevelstackablecore='git://github.com/gimli/server.git Max_level_stackable_core'
@@ -296,32 +305,33 @@ def MaNGOS_Database(host,user,password,work_dir,install_dir,version):
             world.execute("DROP USER 'mangos'@'localhost'")
         else:
             print "\nInstaller is shuttingdown! Please Check the log file for more information!"
-    if del_db=='yes' or del_db=='yes':    
-       print "\nPreparing Databases..."
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/sql/create_mysql.sql')
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' characters < '+work_dir+'/server/sql/characters.sql')
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+work_dir+'/server/sql/mangos.sql')
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' realmd < '+work_dir+'/server/sql/realmd.sql')
-       if version=='tbc':
-          folder='scripts'
-       elif version=='classic':
+    #print "\nSetup new databases?"
+    #del_db=Quest(0)  
+    print "\nPreparing Databases..."
+    os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/sql/create_mysql.sql')
+    os.system('mysql -h '+host+' -u '+user+' -p'+password+' characters < '+work_dir+'/server/sql/characters.sql')
+    os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+work_dir+'/server/sql/mangos.sql')
+    os.system('mysql -h '+host+' -u '+user+' -p'+password+' realmd < '+work_dir+'/server/sql/realmd.sql')
+    if version=='tbc':
+       folder='scripts'
+    elif version=='classic':
           folder='ScriptDevZero'
-       elif version=='cata':
+        elif version=='cata':
           folder='ScriptDev2'
-       elif version=='wotlk':
+        elif version=='wotlk':
           folder='ScriptDev2'
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_database.sql')
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_structure_mysql.sql')
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_script_full.sql')
-       print "\nCreateing full database.."
-       os.system('cd '+install_dir+'/database/database;sh make_full_db.sh')
-       os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+install_dir+'/database/database/full_db.sql')
-       if version=='tbc':
+        os.system('mysql -h '+host+' -u '+user+' -p'+password+' < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_database.sql')
+        os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_create_structure_mysql.sql')
+        os.system('mysql -h '+host+' -u '+user+' -p'+password+' scriptdev2 < '+work_dir+'/server/src/bindings/'+folder+'/sql/scriptdev2_script_full.sql')
+        print "\nCreateing full database.."
+        os.system('cd '+install_dir+'/database/database;sh make_full_db.sh')
+        os.system('mysql -h '+host+' -u '+user+' -p'+password+' mangos < '+install_dir+'/database/database/full_db.sql')
+        if version=='tbc':
           # Minor fix for TBC this is only temperary!
           db = MySQLdb.connect(host,user,password,'mangos')
           cursor = db.cursor()
           cursor.execute('ALTER TABLE db_version CHANGE COLUMN required_12195_02_mangos_mangos_string required_s1718_12113_01_mangos_spell_template bit')
-       print "\nDatabase setup done."
+        print "\nDatabase setup done."
     else:
        print "\nSkipping, Reason: Using current installed databases!"
 
