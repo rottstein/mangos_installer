e58414232bd60a67fe63eb49ccee348f9206673f
Nickless <admin@isengard.dk>
2012-12-23 18:59:43 +0100

    Clean version of predefines.py including info about the values whitin.

--- core/predefines.py
+++ core/predefines.py
@@ -1,25 +1,29 @@
 #!/usr/env python
 # -*- coding: cp1252 -*- 
+#
+# Simply Edit theese values
+# to your needs and save your self
+# the time of the guide.
 
 def loadPreDefines(self):
-    self.install_dir='/home/bs/mangos/cata'
-    self.q_data_dir='/mnt/Data'
-    self.q_sd2='yes'
-    self.q_mc='yes'
-    self.q_ahbot='yes'
-    self.q_acid='yes'
-    self.q_host='localhost'
-    self.q_user='root'
-    self.q_pass='bjo10ern21'
-    self.q_currentrealm='no'
-    self.q_world='mangos_c_world'
-    self.q_realm='mangos_realmd'
-    self.q_char='mangos_c_characters'
-    self.q_script='mangos_c_scriptdev2'
-    self.q_realmid=2
-    self.q_realmname='cata-clean'
-    self.q_realmport=8086
-    self.q_realmip='192.168.1.12'
-    self.q_cores=4
+    self.install_dir='/home/USER/mangos/Cataclysm' # Path to install
+    self.q_data_dir='/home/USER/mangos/Data'       # Path to Data folder (DBC/MAPS/VMAPS)
+    self.q_sd2='yes'                               # Download Scriptdev2
+    self.q_mc='yes'                                # Cataclysm: Mangchat 1.7.9
+    self.q_ahbot='yes'                             # Activate Ahbot
+    self.q_acid='yes'                              # Download ACID
+    self.q_host='localhost'                        # Database host
+    self.q_user='root'                             # Database user (Default: root)
+    self.q_pass='xxxxxx'                           # Database password
+    self.q_currentrealm='no'                       # Use current realm
+    self.q_world='mangos'                          # World Database name
+    self.q_realm='realmd'                          # Realm Database name
+    self.q_char='characters'                       # Characters Database name
+    self.q_script='scriptdev2'                     # ScriptDev2 Database name
+    self.q_realmid=1                               # Realm ID (only when using multiple realms)
+    self.q_realmname='MaNGOS'                      # Realm Name
+    self.q_realmport=8085                          # Realm Port
+    self.q_realmip='127.0.0.1'                     # Realm Ip
+    self.q_cores=4                                 # CPU cores
     self.del_folder(self.install_dir)
     self.del_folder(self.work_dir)
